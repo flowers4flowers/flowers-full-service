367aab930ba5377cf1f648cc5d422b9a75158c14
Hanneri <hannerikruger@gmail.com>
2025-12-18 12:32:06 +0200

    analytics types files

---
+++ next/app/utility/analytics-events.ts
@@ -0,0 +1,24 @@
+export enum AnalyticsEvent {
+  BUTTON_CLICK = 'button_click',
+  LINK_CLICK = 'link_click',
+  FORM_SUBMIT = 'form_submit',
+  NAVIGATION = 'navigation',
+  SOCIAL_LINK = 'social_link_click',
+  MENU_TOGGLE = 'menu_toggle',
+  // I will add more as needed
+}
+
+export enum AnalyticsCategory {
+  ENGAGEMENT = 'engagement',
+  NAVIGATION = 'navigation',
+  CONVERSION = 'conversion',
+  SOCIAL = 'social',
+}
+
+export interface AnalyticsEventParams {
+  event: AnalyticsEvent
+  category?: AnalyticsCategory
+  label?: string
+  value?: number
+  [key: string]: any
+}
