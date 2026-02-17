d99ed2a507beae03e40ea8debf4f2236ac9e806f
Hanneri <hannerikruger@gmail.com>
2026-02-17 10:10:22 +0200

    added microsoft clarity

--- next/app/layout.js
+++ next/app/layout.js
@@ -49,6 +49,15 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-TNKNK842');`,
           }}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "vioyjktsk2");`,
+          }}
+        />
       </head>
       <AppWrapper>
         <body>
