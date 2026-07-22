df809ae5f12845a10f475488463af5ea28d18cf7
Hanneri <hannerikruger@gmail.com>
2026-07-22 14:33:35 +0200

    404 page

---
+++ next/app/not-found.js
@@ -0,0 +1,12 @@
+// next/app/not-found.js
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center py-32 text-center">
+      <h1 className="text-2xl">Page not found</h1>
+      <p className="mt-2">
+        The page you&apos;re looking for doesn&apos;t exist.
+      </p>
+    </div>
+  );
+}
