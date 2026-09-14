640cf5a1ca3566f5bb1444ee6850edf656b95afb
Hanneri <hannerikruger@gmail.com>
2026-09-14 17:59:59 +0200

    vercel error

---
+++ next/app/error.js
@@ -0,0 +1,39 @@
+"use client";
+
+export default function Error({ error, reset }) {
+  return (
+    <div
+      style={{
+        minHeight: "60vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontFamily: "sans-serif",
+        textAlign: "center",
+        padding: "2rem",
+      }}
+    >
+      <div>
+        <h1 style={{ fontSize: "1.5rem", marginBottom: "0.5rem" }}>
+          Something went wrong loading the page
+        </h1>
+        <p style={{ marginBottom: "1.5rem", color: "#555" }}>
+          This is usually temporary. Please try again in a moment.
+        </p>
+        <button
+          onClick={() => reset()}
+          style={{
+            padding: "0.6rem 1.5rem",
+            fontSize: "1rem",
+            cursor: "pointer",
+            border: "1px solid #333",
+            background: "#fff",
+            borderRadius: "4px",
+          }}
+        >
+          Retry
+        </button>
+      </div>
+    </div>
+  );
+}
